3c1bfc3da7e305ce4792dd7b5c40655a513e0d83
S Peiris <shawnpeiris22@gmail.com>
2026-01-20 05:02:05 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -4,12 +4,12 @@ import CharacterSheet from './pages/CharacterSheet';
 import Characters from './pages/Characters';
 import CreateCharacter from './pages/CreateCharacter';
 import DMTools from './pages/DMTools';
+import Economy from './pages/Economy';
 import Help from './pages/Help';
 import Home from './pages/Home';
 import PlayCampaign from './pages/PlayCampaign';
 import Rules from './pages/Rules';
 import Settings from './pages/Settings';
-import Economy from './pages/Economy';
 import __Layout from './Layout.jsx';
 
 
@@ -20,12 +20,12 @@ export const PAGES = {
     "Characters": Characters,
     "CreateCharacter": CreateCharacter,
     "DMTools": DMTools,
+    "Economy": Economy,
     "Help": Help,
     "Home": Home,
     "PlayCampaign": PlayCampaign,
     "Rules": Rules,
     "Settings": Settings,
-    "Economy": Economy,
 }
 
 export const pagesConfig = {
