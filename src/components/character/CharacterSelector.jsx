86ae3041903ab2fe0ad2b7931dc45c49d9530146
S Peiris <shawnpeiris22@gmail.com>
2026-01-20 17:47:14 +0000

    File changes

--- src/components/character/CharacterSelector.jsx
+++ src/components/character/CharacterSelector.jsx
@@ -40,11 +40,11 @@ export default function CharacterSelector({ characters, onSelect, onClose }) {
           <div className="relative">
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-slate-500" />
             <Input
+              ref={inputRef}
               value={search}
               onChange={(e) => setSearch(e.target.value)}
               placeholder="Search characters..."
               className="pl-10 bg-slate-800 border-slate-700 text-white"
-              autoFocus={false}
             />
           </div>
 
